fc7c64af8337772548b397169c56040b0ea8d57e
Krishna-2311 <goyal.krishna9368@gmail.com>
2025-08-31 00:33:49 +0530

    Count Digit

---
+++ BasicMaths/Count_Digit.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int countdigit(int n) {
+    if (n == 0) return 1;
+    if (n < 0) n = -n;
+
+    int count = 0;
+    while (n > 0) {
+        count++;
+        n = n / 10;
+    }
+    return count;
+}
+
+int main() {
+    int num;
+    cin >> num;
+    cout << "Digits: " << countdigit(num) << endl;
+}
