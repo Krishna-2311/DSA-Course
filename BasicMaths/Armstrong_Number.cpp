c8e326bf3c7043d74e9d3eafb038b9360d6b31a8
Krishna-2311 <goyal.krishna9368@gmail.com>
2025-09-01 12:17:00 +0530

    Armstrong Number

---
+++ BasicMaths/Armstrong_Number.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int Sum=0;
+    int dup=n;
+    while(n>0){
+        int lastdigit=n%10;
+        Sum=Sum+(lastdigit*lastdigit*lastdigit);
+        n=n/10;
+    }
+    if(Sum==dup){
+        cout<<"Yes, It is Armstrong Number"<<endl;
+    }
+    else{
+              cout<<"No, It is Not a Armstrong Number"<<endl;
+    }
+}
