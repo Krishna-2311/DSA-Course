4f81541282059e95a6badf89df48580a680ae822
Krishna-2311 <goyal.krishna9368@gmail.com>
2025-09-01 19:51:45 +0530

    Added Print All Number

--- 01.BasicMaths/PrintAllDivisor.cpp
+++ 01.BasicMaths/PrintAllDivisor.cpp
@@ -1,11 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     int n;
-    cin>>n;
-    for(int i=1; i<=n;i++){
-        if(n%i==0){
-            cout<<i<<endl;
+    cout << "Please Enter the Number" << endl;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            cout << i << endl;
         }
     }
 }
