4fead10bdaa82a828ba7e546a9b1bdff99cc4fc6
Krishna-2311 <goyal.krishna9368@gmail.com>
2025-09-01 19:18:48 +0530

    Added GCD

---
+++ BasicMaths/GCD.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a;
+    cout<<"Enter the value of a "<<endl;
+    cin>>a;
+    int b;
+    cout<< " Enter the  value of b"<<endl;
+    cin>>b;
+    while(a>0 && b>0){
+        if(a>b){
+            a=a%b;
+        }
+        else{
+            b=b%a;
+        }
+    }
+    if(a==0){
+        cout <<"The GCD is"<<" " <<b<<endl;
+    }
+    else
+    { cout <<"the GCD is" <<" "<<a<<endl;}
+    return 0;
+}
