24bf38a8e0215ebb840b02b49bcf67ef5bade824
Krishna-2311 <goyal.krishna9368@gmail.com>
2025-09-01 19:52:32 +0530

    Added Prime Number

--- 01.BasicMaths/PrimeNum.cpp
+++ 01.BasicMaths/PrimeNum.cpp
@@ -1,26 +1,32 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main() {
+int main()
+{
     int n;
-    cout<<"Enter the number"<<endl;
-    cin>>n;
-         if(n==0||n==1){
-            cout<<"Not the prime number"<<endl;
-              return 0;
-         }
-          
-         int count=0;
-        for(int i=1; i<=n;i++){
-            if( n % i==0){
-                count++;
-            }
+    cout << " Please Enter the number" << endl;
+    cin >> n;
+    if (n == 0 || n == 1)
+    {
+        cout << "Not the prime number" << endl;
+        return 0;
+    }
+
+    int count = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            count++;
         }
-            if(count ==2){
-                cout<<"It is a prime number"<<endl;
-            }
-            else{
-                cout<<"It is not a prime number"<<endl;
-            }
-        
+    }
+    if (count == 2)
+    {
+        cout << "It is a prime number" << endl;
+    }
+    else
+    {
+        cout << "It is not a prime number" << endl;
+    }
+
     return 0;
 }
