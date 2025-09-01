bd60098a8d4f79194519dda70a5db68ac8253f54
Krishna-2311 <goyal.krishna9368@gmail.com>
2025-09-01 11:17:01 +0530

    Added Check palindrome

---
+++ BasicMaths/Check_Palindrome.cpp
@@ -0,0 +1,19 @@
+# include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int dup=n;
+    int revNum=0;
+    while(n>0){
+        int lastdigit=n%10;
+        n=n/10;
+    revNum=(revNum*10)+lastdigit;
+    }
+    if(revNum==dup){
+        cout<<"Yes this is a palindrome"<<endl;
+    }
+    else{
+        cout<<"no"<<endl;
+    }
+}
