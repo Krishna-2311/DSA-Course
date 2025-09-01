809496dcc29de48bb893a5a7873d8aae238288fd
Krishna-2311 <goyal.krishna9368@gmail.com>
2025-09-01 19:53:25 +0530

    Added GCD

--- 01.BasicMaths/GCD.cpp
+++ 01.BasicMaths/GCD.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main() {
     int a;
-    cout<<"Enter the value of a "<<endl;
+    cout<<" Please Enter the value of a "<<endl;
     cin>>a;
     int b;
-    cout<< " Enter the  value of b"<<endl;
+    cout<< " Please Enter the  value of b"<<endl;
     cin>>b;
     while(a>0 && b>0){
         if(a>b){
