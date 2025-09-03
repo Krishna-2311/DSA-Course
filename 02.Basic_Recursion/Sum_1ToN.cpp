6c07aa974112b2e7ca9602a19eebff30135b81cb
Krishna-2311 <goyal.krishna9368@gmail.com>
2025-09-03 19:12:21 +0530

    Added Sum From first to n

---
+++ 02.Basic_Recursion/Sum_1ToN.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void fun(int i, int sum)
+{
+    if (i < 1)
+    {
+        cout << sum << endl;
+        return;
+    }
+    fun(i - 1, sum + i);
+}
+
+int main()
+{
+    int n;
+    cout << "Please Enter the number" << endl;
+    cin >> n;
+    fun(n, 0);
+
+    return 0;
+}
