671a9890e33e4218cd3d10b00d4b7de67847c836
Krishna-2311 <goyal.krishna9368@gmail.com>
2025-09-04 23:55:37 +0530

    Added Selection Sort

---
+++ 03.Sorting/Selection_Sort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+ void Selection_Sort(int arr[], int n){
+        for(int i=0; i<=n-2; i++){
+            int mini=i;
+    
+        for (int j=i; j<=n-1; j++){
+         if(arr[j]<arr[mini]){
+            mini=j;
+         }
+        }
+        int temp=arr[mini];
+        arr[mini]=arr[i];
+        arr[i]=temp;
+    }
+}
+int main() { 
+    int n;
+    cout<<"please enter the number"<<endl;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];}
+        Selection_Sort(arr,n);
+        for (int i=0; i<n;i++){
+            cout<<arr[i]<<endl;
+        }
+    
+    return 0;
+}
