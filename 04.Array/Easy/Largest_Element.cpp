d56d5e8d690946d9799f2acab6cea002a37cbd56
Krishna-2311 <goyal.krishna9368@gmail.com>
2025-09-07 15:42:45 +0530

    Added largest Element

---
+++ 04.Array/Easy/Largest_Element.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Please Enter the number"<<endl;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n;i++){
+        cin>>arr[i];
+    }
+    int largest=arr[0];
+    for(int i=1;i<n;i++){
+        if(arr[i]>largest){
+            largest=arr[i];
+
+        }
+        
+    }
+    cout<<largest<<endl;
+    
+    
+    return 0;
+}
